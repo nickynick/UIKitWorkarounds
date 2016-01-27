ba7b5b502d274792f6e724886dfabe228436e3b2
Nick Tymchenko <t.nick.a@gmail.com>
2016-01-28 01:06:28 +0300

    Converted to dynamic framework

--- UIKitWorkarounds/UIKitWorkarounds.h
+++ UIKitWorkarounds/UIKitWorkarounds.h
@@ -2,12 +2,18 @@
 //  UIKitWorkarounds.h
 //  UIKitWorkarounds
 //
-//  Created by Nick Tymchenko on 15/01/16.
+//  Created by Nick Tymchenko on 28/01/16.
+//  Copyright © 2016 Nick Tymchenko. All rights reserved.
 //
-//
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface UIKitWorkarounds : NSObject
+//! Project version number for UIKitWorkarounds.
+FOUNDATION_EXPORT double UIKitWorkaroundsVersionNumber;
 
-@end
+//! Project version string for UIKitWorkarounds.
+FOUNDATION_EXPORT const unsigned char UIKitWorkaroundsVersionString[];
+
+
+#import <UIKitWorkarounds/NNTableViewReloader.h>
+#import <UIKitWorkarounds/NNCollectionViewReloader.h>
